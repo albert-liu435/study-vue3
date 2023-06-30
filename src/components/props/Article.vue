c4cd2704db8cf99c4cc7a183a781d2b3d73bd22f
albert.liu435 <albert.liu435@gmail.com>
2023-06-30 23:54:33 +0800

    day05

---
+++ src/components/props/Article.vue
@@ -0,0 +1,26 @@
+<script setup>
+//通过definProps "编译器宏" 接收子组件传递的数据,外界可以传递指定的数据
+const props = defineProps({
+  title: {
+    type: String,
+    //default: 'abc',
+    require: true
+  },
+  author: String,
+  pubTime: String
+})
+</script>
+
+
+<template>
+  <div>
+    <h3>标题：{{props.title}}</h3>
+    <h5>作者：{{props.author}}</h5>
+    <h6>发布时间：{{props.pubTime}}</h6>
+  </div>
+</template>
+
+
+
+<style scoped>
+</style>
